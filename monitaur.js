00c404aebfe2440da994aeaea6ab795025838295
Jeremy Coulter <jeremycoulter@Jeremys-MacBook-Pro.local>
2019-02-13 00:25:39 -0500

    Quickies now include REMERON with PRISTIQ.

--- monitaur.js
+++ monitaur.js
@@ -1,5 +1,5 @@
 /*!
- * monitaur v0.0.29 (https://github.com/jeremycoulter/monitaur)
+ * monitaur v0.0.30 (https://github.com/jeremycoulter/monitaur)
  * Copyright 2019 Jeremy Coulter (https://jeremycoulter.github.io)
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  */
@@ -7,7 +7,7 @@
 /*
  * constants
  */
-var CURRENT_VERSION = "0.0.29";
+var CURRENT_VERSION = "0.0.30";
 
 var DEVELOPER_API_KEY = "67717ca4d7209a13a5e9061c3d0a58f5";
 var DEVELOPER_TOKEN = "53a3e070da0bcb2ef5701a96ebe5e817dc9d308dd1d04c61d84d58f1caae05e6";
@@ -97,7 +97,13 @@ function printTrelloCards() {
                     for(var j = 0; j < Object.keys(quickies).length; j++) {
                         if(cardDescription.toLowerCase().includes(Object.keys(quickies)[j])) {
                             quickies[Object.keys(quickies)[j]]++;
+                            console.log("hey");
                         }
+                    }
+
+                    // TODO temporary fix to account for new medication
+                    if(cardDescription.toLowerCase() === "remeron") {
+                        quickies["pristiq"]++;
                     }
 
                 }
