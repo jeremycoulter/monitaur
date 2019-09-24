e206eec2156994f943824c81b956f6afe1412eab
Jeremy Coulter <jeremycoulter@Jeremys-MacBook-Pro.local>
2019-09-24 11:12:29 -0400

    Fixed BLEXTEN icon and version number.

--- monitaur.js
+++ monitaur.js
@@ -1,5 +1,5 @@
 /*!
- * monitaur v0.0.42 (https://github.com/jeremycoulter/monitaur)
+ * monitaur v0.0.44 (https://github.com/jeremycoulter/monitaur)
  * Copyright 2019 Jeremy Coulter (https://jeremycoulter.github.io)
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  */
@@ -7,7 +7,7 @@
 /*
  * constants
  */
-var CURRENT_VERSION = "0.0.43";
+var CURRENT_VERSION = "0.0.44";
 
 var DEVELOPER_API_KEY = "67717ca4d7209a13a5e9061c3d0a58f5";
 var DEVELOPER_TOKEN = "53a3e070da0bcb2ef5701a96ebe5e817dc9d308dd1d04c61d84d58f1caae05e6";
